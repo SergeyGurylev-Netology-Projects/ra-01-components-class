89d6c67aea9f7d68e099da2c43fe51e13beaba5b
SergeyGurylev <ohg@mail.ru>
2024-02-07 13:01:34 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class ShopItemClass extends React.Component {
+  constructor(props) {
+    super();
+    this.props = props;
+  }
+
+  render() {
+    return (
+      <div className="main-content">
+        <h2>{this.props.brand}</h2>
+        <h1>{this.props.title}</h1>
+        <h3>{this.props.description}</h3>
+        <div className="description">
+          {this.props.descriptionFull}
+        </div>
+        <div className="highlight-window mobile">
+          <div className="highlight-overlay"></div>
+        </div>
+        <div className="divider"></div>
+        <div className="purchase-info">
+          <div className="price">{this.props.currency}{this.props.price.toFixed(2)}</div>
+          <button>Добавить в корзину</button>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default App;
+export default ShopItemClass ;
